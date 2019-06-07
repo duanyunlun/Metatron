6b1dd849a1127b74f1cdf30d3d2bb75ac634b8a3
Megaflan <smtivesp@gmail.com>
2019-06-07 20:20:59 +0200

    Agregar archivos de proyecto.

---
+++ Metatron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metatron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
